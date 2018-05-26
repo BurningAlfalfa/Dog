99c9211df62d7bbc02df0483916eb06cbdb08da8
BurningAlfalfa <acmonahan@gmail.com>
2018-05-25 18:32:37 -0700

    added adddog method and print dog method

--- DogMain.java
+++ DogMain.java
@@ -3,7 +3,7 @@ public class DogMain {
 
         Dog dog1 = new Dog("Lika", 7, false);
 
-        System.out.println(dog1.getName());
+        //System.out.println(dog1.getName());
         Cat cat1 = new Cat ("mittens", true,6);
         Dog dog2 = new Dog ("Leo", 5);
         Dog dog3 = new Dog ("Frank", 4);
@@ -11,18 +11,21 @@ public class DogMain {
         Cat cat2 = new Cat ("jingles", 7);
         Cat cat3 = new Cat ("finger",3);
         Cat cat4 = new Cat ("squib",3);
-        dog1.setName(dog2.getName());
 
-        Dog dogArray[] = {dog1,dog2,dog3,dog4};
+       Dog dogArray[] = {dog1,dog2,dog3,dog4};
         Cat catArray[] = {cat2,cat3,cat4};
-        System.out.println(dog2.getName()+" "+ dog2.getAge());
+        /*System.out.println(dog2.getName()+" "+ dog2.getAge());
         System.out.println(dog1.printName());
         System.out.println(dog1.getName());
-        System.out.println(dogArray[0].getName());
+        System.out.println(dogArray[0].getName());*/
 
-
-
-        System.out.println(cat1.humanAge());
+        Kennel kennel = new Kennel("Bobs");
+        kennel.addDog(dog1);
+        kennel.printDogs();
+        kennel.addDog(dog2);
+        kennel.removeDog(dog1.getName());
+        kennel.printDogs();
+        //System.out.println(cat1.humanAge());
 
     }
 }
--- Kennel.java
+++ Kennel.java
@@ -1,16 +1,70 @@
 public class Kennel {
-    private String kName;
+    private String name;
+    private Dog [] dogArray;
+    private Cat [] catArray;
+    public void setkName(String name){
+        this.name= name;
+    }
+    public void setDogArray(Dog [] dogArray ){
+        this.dogArray = dogArray;
+    }
+    public void setCatArray(Cat [] catArray){
+        this.catArray = catArray;
+    }
+    public Dog []getDogArray(){
+            return dogArray;
+    }
+    public Cat []getCatArray(){
+        return catArray;
+    }
+    public Kennel(String name){
+        this.name = name;
+        this.dogArray = new Dog[0];
+        this.catArray = new Cat[0];
 
-    public void setkName(String kName){
-        this.kName = kName;
+    }
+    public void addDog(Dog dog){
+       Dog[] newDogArray = new Dog[dogArray.length+1];
+       for(int i = 0; i < dogArray.length;i++){
+            newDogArray[i] = dogArray[i];
+
+       }
+       newDogArray[newDogArray.length-1] = dog;
+       this.dogArray = newDogArray;
+       //System.out.println(dog.getName());
+    }
+    public void printDogs(){
+        //System.out.println("the length of the dog array is "+ dogArray.length);
+        System.out.println("printing dogs");
+        for(int i = 0; i < dogArray.length;i++){
+            System.out.println("name : " + dogArray[i].getName());
+        }
+        System.out.println("done");
+    }
+    public void removeDog(String dogName){
+        /*Dog[] newDogArray = new Dog[dogArray.length-1];
+        for(int i = 0;i < dogArray.length;i++){
+            newDogArray[i] = dogArray[i];
+        }
+
+        this.dogArray = newDogArray;*/
+        for(int i = 0;i < dogArray.length;i++){
+            if(dogArray[i].getName().equals(dogName)){
+                dogArray[i].setName("fuck");
+            }
+
+        }
+    }
+    public void addCat(Cat cat){
+
+    }
+    public void removeCat(){
+
     }
 
 
-
-
-
 }
-//so kennal call shiould have its field variable, should be an array of dogs, an array of cats
+//so kennal  shiould have its field variable, should be an array of dogs, an array of cats
 //the name of the kennel,
 //methods, getters, setter, prints all dog name on seperate line, and ages , method prints all cat names on seperate lines and ages
 // methods 4, one to add and remove both cat and dogp
